c866806922067f390ea83c69a4e11eeb1ab17118
ronglian <ronglian@201711290915-PC>
2018-01-10 10:16:56 +0800

    用户dislike持久层操作

---
+++ src/main/java/com/ronglian/controller/ContentDislikeController.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.ronglian.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ronglian.service.ContentDislikeService;
+import com.ronglian.utils.RongLianResult;
+
+/**
+ * @author liyang
+ * @createTime 2017年12月29日
+ */
+@RestController
+@RequestMapping("/api")
+public class ContentDislikeController {
+
+	/**
+	 * 用户对内容进行disLike操作
+	 * @param newsId
+	 * @param deviceId
+	 * @param userId
+	 * @return
+	 */
+	@Autowired
+	private ContentDislikeService contentDislikeService;
+	@RequestMapping(value="/1.0/contentdislike",method=RequestMethod.POST)
+	public RongLianResult contentDisLike(String newsId,String deviceId,String userId){
+		return RongLianResult.ok();
+	}
+}
