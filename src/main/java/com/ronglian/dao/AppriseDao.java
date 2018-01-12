d4c1da07840bb0d8f2adee195e9b5537a993abbc
ronglian <ronglian@201711290915-PC>
2018-01-12 10:33:41 +0800

    点赞Dao层

---
+++ src/main/java/com/ronglian/dao/AppriseDao.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.ronglian.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.ronglian.entity.NewsInfoApprise;
+
+/**
+ * @author liyang
+ * @createTime 2018Äê1ÔÂ11ÈÕ
+ */
+public interface AppriseDao extends CrudRepository<NewsInfoApprise, String> {
+
+	/**
+	 * @param deviceId
+	 * @param userId
+	 * @param newsId
+	 * @return
+	 */
+	@Query("select  apprise from NewsInfoApprise apprise where deviceId = :deviceId and userId = :userId and newsId = :newsId")
+	public NewsInfoApprise getNewsInfoApprise(@Param("deviceId")String deviceId, @Param("userId")String userId,@Param("newsId") String newsId);
+
+	@Query("select  apprise from NewsInfoApprise apprise where deviceId = :deviceId and userId = :userId")
+	public List<NewsInfoApprise> selectAppriseList(@Param("deviceId")String deviceId,@Param("userId")String userId);
+}
