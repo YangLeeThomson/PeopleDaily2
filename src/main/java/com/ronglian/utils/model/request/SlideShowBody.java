65f190ce4328f8af6c6a5f3a2dc7bb1036bb6bf4
YehoLee <33524422+YehoLee@users.noreply.github.com>
2018-02-05 09:58:15 +0800

    修改轮播图返回格式

---
+++ src/main/java/com/ronglian/utils/model/request/SlideShowBody.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package com.ronglian.utils.model.request;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+/**
+ * @author liyang
+ * @createTime 2018Äê2ÔÂ2ÈÕ
+ */
+public class SlideShowBody implements Serializable{
+		private static final long serialVersionUID = 1L;
+		private String slideShowId;
+		private String channelUniqueId;
+//		private Date createTime;
+		private String createTime;
+		private String imageUrl;
+		private String newsId;
+		private String title;
+		private String desc;
+		private String dataStatus;
+		private Integer sort;
+		public String getSlideShowId() {
+			return slideShowId;
+		}
+		public void setSlideShowId(String slideShowId) {
+			this.slideShowId = slideShowId;
+		}
+		public String getChannelUniqueId() {
+			return channelUniqueId;
+		}
+		public void setChannelUniqueId(String channelUniqueId) {
+			this.channelUniqueId = channelUniqueId;
+		}
+		public String getCreateTime() {
+			return createTime;
+		}
+		public void setCreateTime(String createTime) {
+			this.createTime = createTime;
+		}
+		public String getImageUrl() {
+			return imageUrl;
+		}
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+		public String getNewsId() {
+			return newsId;
+		}
+		public void setNewsId(String newsId) {
+			this.newsId = newsId;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getDesc() {
+			return desc;
+		}
+		public void setDesc(String desc) {
+			this.desc = desc;
+		}
+		public String getDataStatus() {
+			return dataStatus;
+		}
+		public void setDataStatus(String dataStatus) {
+			this.dataStatus = dataStatus;
+		}
+		public Integer getSort() {
+			return sort;
+		}
+		public void setSort(Integer sort) {
+			this.sort = sort;
+		}
+		
+}
