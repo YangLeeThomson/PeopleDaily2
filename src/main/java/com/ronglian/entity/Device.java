0e8f5a0256752bc3f47e7a9138b5ad5491d6e663
liyang <liyang03@ronglian>
2018-07-26 20:06:57 +0800

    更改device类字段名称

--- src/main/java/com/ronglian/entity/Device.java
+++ src/main/java/com/ronglian/entity/Device.java
@@ -42,7 +42,7 @@ public class Device implements Serializable{
 	 * iOS 设备传入idfa
 	 * */
 	@Column(name="id_type")
-	private String idType;
+	private String id_type;
 	/*
 	 * 广告追踪 
 	 * 设备的“限制广告跟踪”状态。
@@ -56,26 +56,26 @@ public class Device implements Serializable{
 	 * Android 设备:
 	 * */
 	@Column(name="app_version")
-	private String appVersion;
+	private String app_version;
 	/*
 	 * 系统类型，
 	 * 固定值： iOS  或者 Android
 	 * */
 	
 	@Column(name="os_type")
-	private String osType;
+	private String os_type;
 	/*
 	 * 系统版本
 	 * */
 	@Column(name="os_version")
-	private String osVersion;
+	private String os_version;
 	
 	/*
 	 * 如果应用未使用 SDK，
 	 * 请传递与 app_version 相同的值
 	 * */
 	@Column(name="sdk_version")
-	private String sdkVersion;
+	private String sdk_version;
 	/*
 	 * 必须 首次打开时间的 UNIX 时间戳
 	 * （以秒为单位，精确到微秒）
@@ -83,6 +83,20 @@ public class Device implements Serializable{
 	@Column(name="timestamp")
 	private Float timestamp;
 	
+
+	/*
+	 * 记录创建时间
+	 * */
+	@Column(name="create_time")
+	private Date create_time;
+	
+	/*	
+	 * 调用google接口返回结果,
+	 * 0或者1 允许空
+	 * */
+	@Column(name="push_result")
+	private Integer push_result;
+
 	public String getRdid() {
 		return rdid;
 	}
@@ -91,12 +105,12 @@ public class Device implements Serializable{
 		this.rdid = rdid;
 	}
 
-	public String getIdType() {
-		return idType;
+	public String getId_type() {
+		return id_type;
 	}
 
-	public void setIdType(String idType) {
-		this.idType = idType;
+	public void setId_type(String id_type) {
+		this.id_type = id_type;
 	}
 
 	public Integer getLat() {
@@ -107,36 +121,37 @@ public class Device implements Serializable{
 		this.lat = lat;
 	}
 
-	public String getAppVersion() {
-		return appVersion;
+	public String getApp_version() {
+		return app_version;
 	}
 
-	public void setAppVersion(String appVersion) {
-		this.appVersion = appVersion;
+	public void setApp_version(String app_version) {
+		this.app_version = app_version;
 	}
 
-	public String getOsType() {
-		return osType;
+	public String getOs_type() {
+		return os_type;
 	}
 
-	public void setOsType(String osType) {
-		this.osType = osType;
+	public void setOs_type(String os_type) {
+		this.os_type = os_type;
 	}
 
-	public String getOsVersion() {
-		return osVersion;
+
+	public String getOs_version() {
+		return os_version;
 	}
 
-	public void setOsVersion(String osVersion) {
-		this.osVersion = osVersion;
+	public void setOs_version(String os_version) {
+		this.os_version = os_version;
 	}
 
-	public String getSdkVersion() {
-		return sdkVersion;
+	public String getSdk_version() {
+		return sdk_version;
 	}
 
-	public void setSdkVersion(String sdkVersion) {
-		this.sdkVersion = sdkVersion;
+	public void setSdk_version(String sdk_version) {
+		this.sdk_version = sdk_version;
 	}
 
 	public Float getTimestamp() {
@@ -147,38 +162,21 @@ public class Device implements Serializable{
 		this.timestamp = timestamp;
 	}
 
-	public Date getCreateTime() {
-		return createTime;
+	public Date getCreate_time() {
+		return create_time;
 	}
 
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
+	public void setCreate_time(Date create_time) {
+		this.create_time = create_time;
 	}
 
-	public Integer getPushResult() {
-		return pushResult;
+	public Integer getPush_result() {
+		return push_result;
 	}
 
-	public void setPushResult(Integer pushResult) {
-		this.pushResult = pushResult;
+	public void setPush_result(Integer push_result) {
+		this.push_result = push_result;
 	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-	/*
-	 * 记录创建时间
-	 * */
-	@Column(name="create_time")
-	private Date createTime;
-	
-	/*	
-	 * 调用google接口返回结果,
-	 * 0或者1 允许空
-	 * */
-	@Column(name="push_result")
-	private Integer pushResult;
 	
 	
 	
--- src/main/java/com/ronglian/service/impl/GoogleServiceImpl.java
+++ src/main/java/com/ronglian/service/impl/GoogleServiceImpl.java
@@ -59,7 +59,9 @@ public class GoogleServiceImpl implements GoogleService {
 		}
 		//请求谷歌服务
 		String requestGoogleUrl = preparedGoogleURL(RongLianConstant.GOOGLE_ADWORDS_URL, device);
+		System.out.println("requestGoogleUrl==== "+requestGoogleUrl);
 		String resultStr = HttpClientUtil.doPost(requestGoogleUrl, null);
+		System.out.println("resultStr==== "+resultStr);
 		//解析google返回数据
 		ObjectMapper mapper = new ObjectMapper();
 		Map mapRes;
@@ -68,14 +70,14 @@ public class GoogleServiceImpl implements GoogleService {
 			boolean attributed = (boolean) mapRes.get("attributed");
 			List<DeviceFromadwords> ad_events = null;
 			if(attributed){
-				device.setPushResult(1);
+				device.setPush_result(1);
 				ad_events = (List<DeviceFromadwords>) mapRes.get("ad_events");
 				DeviceFromadwords deviceFrowords = ad_events.get(0);
 				DeviceFromadwords returnWordsTntity = this.deviceFromadwordsDao.save(deviceFrowords);
 				returnEntity = this.deviceDao.save(device);
 				return RongLianResult.ok();
 			}else{
-				device.setPushResult(0);
+				device.setPush_result(0);
 				returnEntity = this.deviceDao.save(device);
 				return RongLianResult.ok();
 			}
@@ -103,23 +105,23 @@ public class GoogleServiceImpl implements GoogleService {
 		if(flag){
 			return true;
 		}
-		flag = flag || StringUtils.isBlank(device.getIdType());
+		flag = flag || StringUtils.isBlank(device.getId_type());
 		if(flag){
 			return true;
 		}
-		flag = flag || StringUtils.isBlank(device.getAppVersion());
+		flag = flag || StringUtils.isBlank(device.getApp_version());
 		if(flag){
 			return true;
 		}
-		flag = flag || StringUtils.isBlank(device.getOsType());
+		flag = flag || StringUtils.isBlank(device.getOs_type());
 		if(flag){
 			return true;
 		}
-		flag = flag || StringUtils.isBlank(device.getOsVersion());
+		flag = flag || StringUtils.isBlank(device.getOs_version());
 		if(flag){
 			return true;
 		}
-		flag = flag || StringUtils.isBlank(device.getSdkVersion());
+		flag = flag || StringUtils.isBlank(device.getSdk_version());
 		if(flag){
 			return true;
 		}
@@ -138,19 +140,19 @@ public class GoogleServiceImpl implements GoogleService {
 		String dev_token = RongLianConstant.DEV_TOKEN;
 		String link_id = null;
 		//固定值： iOS  或者 Android
-		if("iOS".equals(device.getOsType())){
+		if("iOS".equals(device.getOs_type())){
 			link_id = RongLianConstant.iOS_KEY;
 		}
-		if("Android".equals(device.getOsType())){
+		if("Android".equals(device.getOs_type())){
 			link_id = RongLianConstant.Android_KEY;
 		}
 		String app_event_type = RongLianConstant.APP_EVENT_TYPE;
 		String rdid = device.getRdid();
-		String id_type = device.getIdType();
+		String id_type = device.getId_type();
 		int lat = device.getLat();
-		String app_version = device.getAppVersion();
-		String os_version = device.getOsVersion();
-		String sdk_version = device.getSdkVersion();
+		String app_version = device.getApp_version();
+		String os_version = device.getOs_version();
+		String sdk_version = device.getSdk_version();
 		float timestamp = device.getTimestamp();
 		
 		Map<String,String> map = new HashMap<String, String>();
