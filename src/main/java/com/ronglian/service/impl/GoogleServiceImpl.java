679233418814e9c254a0a54922376c3207548d16
liyang <liyang03@ronglian>
2018-07-27 14:04:34 +0800

    人民日报二期，追加推送Google用户设备信息

--- src/main/java/com/ronglian/service/impl/GoogleServiceImpl.java
+++ src/main/java/com/ronglian/service/impl/GoogleServiceImpl.java
@@ -66,12 +66,12 @@ public class GoogleServiceImpl implements GoogleService {
 		Map mapRes;
 		try {
 			String resultStr = HttpClientUtil.doPost(requestGoogleUrl, null);
-			resultStr = "{\"ad_events\": [{\"ad_event_id\": \"Q2owS0VRancwZHk0QlJDdXVMX2U1TQ\",\"conversion_metric\": \"conversion\",\"interaction_type\": \"click\","+
+			/*resultStr = "{\"ad_events\": [{\"ad_event_id\": \"Q2owS0VRancwZHk0QlJDdXVMX2U1TQ\",\"conversion_metric\": \"conversion\",\"interaction_type\": \"click\","+
 						"\"campaign_type\": \"Search\",\"campaign_id\": 123456789,\"campaign_name\": \"My App Campaign\","+
 						"\"ad_type\": \"ClickToDownload\",\"external_customer_id\": 123456789,\"location\": 21144,"+
 						"\"network_type\": \"Search\",\"network_subtype\": \"GoogleSearch\",\"video_id\": null,"+
 						"\"keyword\": \"+space +birds\",\"match_type\": \"b\",\"placement\": null,\"ad_group_id\": 123456789,"+
-						"\"creative_id\": 123456789,\"timestamp\": 1432681913.123456}],\"errors\": [],\"attributed\": true}";
+						"\"creative_id\": 123456789,\"timestamp\": 1432681913.123456}],\"errors\": [],\"attributed\": true}";*/
 			mapRes = mapper.readValue(resultStr, Map.class);
 			boolean attributed = (boolean) mapRes.get("attributed");
 			List<Map> ad_events = null;
